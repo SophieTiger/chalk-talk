bad72be0a687e4ff152d92aaf14c4b58aa7c5123
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-11-18 09:37:08 +0000

    Add SignUpForm and styling

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://chalk-talk-api-7f804e82f4b9.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
