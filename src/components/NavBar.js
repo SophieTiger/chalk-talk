59ee0c719bea02de58851c009b1f347eb8b6e657
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-11-18 15:22:29 +0000

    Add context to current user and set navbar links to reflect user login state

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import chalk_logo from '../assets/chalk_logo.png'
 import styles from '../styles/NavBar.module.css';
 import { NavLink } from 'react-router-dom';
+import { CurrentUserContext } from '../App';
 
 const NavBar = () => {
+    const currentUser = useContext(CurrentUserContext);
+    const loggedInIcons = <>{currentUser?.username}</>
+    const loggedOutIcons = (
+        <>
+            <NavLink className={styles.Navlink} activeClassName={styles.Active} to="/signin">
+                <i className='fas fa-sign-in-alt'></i> Sign In
+            </NavLink>
+            <NavLink className={styles.Navlink} activeClassName={styles.Active} to="/signup">
+                <i className='fas fa-user-plus'></i> Sign Up
+            </NavLink>
+        </>
+    );
+
     return (
         <Navbar className={styles.NavBar} expand="md" fixed='top'>
             <Container>
                 <NavLink to="/">
-                <Navbar.Brand>
-                    <img src={chalk_logo} alt='logo' height='80' />
-                </Navbar.Brand></NavLink>
+                    <Navbar.Brand>
+                        <img src={chalk_logo} alt='logo' height='80' />
+                    </Navbar.Brand></NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
                         <NavLink exact className={styles.Navlink} activeClassName={styles.Active} to="/">
                             <i className='fas fa-home'></i> Home
-                        </NavLink>
-                        <NavLink className={styles.Navlink} activeClassName={styles.Active} to="/signin">
-                            <i className='fas fa-sign-in-alt'></i> Sign In
-                        </NavLink>
-                        <NavLink className={styles.Navlink} activeClassName={styles.Active} to="/signup">
-                            <i className='fas fa-user-plus'></i> Sign Up
-                        </NavLink>
+                        </NavLink >
+                        {currentUser ? loggedInIcons : loggedOutIcons}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useContext, useState} from "react";
 
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
@@ -13,36 +13,87 @@ import { Link } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
+import axios from "axios";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { SetCurrentUserContext } from "../../App";
 
 function SignInForm() {
-    //   Add your component logic here
+    const setCurrentUser = useContext(SetCurrentUserContext)
+    const [signInData, setSignInData] = useState({ username: "", password: "", });
+
+    const { username, password } = signInData;
+    const history = useHistory();
+    const [errors, setErrors] = useState({});
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const {data} = await axios.post("/dj-rest-auth/login/", signInData);
+            setCurrentUser(data.user)
+            history.push("/");
+        } catch (err) {
+            setErrors(err.response?.data);
+        }
+    };
+
+    const handleChange = (event) => {
+        setSignInData({
+          ...signInData,
+          [event.target.name]: event.target.value,
+        });
+      };
 
     return (
         <Row className={styles.Row}>
             <Col className="my-auto p-0 p-md-2" md={6}>
                 <Container className={`${appStyles.Content} p-4 `}>
                     <h1 className={styles.Header}>sign in</h1>
-                    <Form>
+                    <Form onSubmit={handleSubmit}>
                         <Form.Group controlId="username">
                             <Form.Label className="d-none">username</Form.Label>
                             <Form.Control
+                                className={styles.Input}
                                 type="text"
                                 placeholder="Enter Username"
                                 name="username"
+                                value={username}
+                                onChange={handleChange}
                             />
                         </Form.Group>
+                        {errors.username?.map((message, idx) => (
+                            <Alert variant="danger" key={idx}>
+                                {message}
+                            </Alert>
+                        ))}
 
                         <Form.Group controlId="password">
                             <Form.Label className="d-none">Password</Form.Label>
                             <Form.Control
+                                className={styles.Input}
                                 type="password"
                                 placeholder="Password"
                                 name="password"
+                                value={password}
+                                onChange={handleChange}
                             />
                         </Form.Group>
-                        <Button variant="primary" type="submit">
-                            Submit
+                        {errors.password?.map((message, idx) => (
+                            <Alert key={idx} variant="danger">
+                                {message}
+                            </Alert>
+                        ))}
+
+                        <Button
+                            className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+                            type="submit">
+                            Sign in
                         </Button>
+                        {errors.non_field_errors?.map((message, idx) => (
+                            <Alert key={idx} variant="danger" className="mt-3">
+                                {message}
+                            </Alert>
+                        ))}
                     </Form>
 
                 </Container>
