b0898305d44dc6075f988974737f7e6ce1b498d5
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-11-18 08:12:28 +0000

    Install react-router-dom and add routing to app.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,26 +2,28 @@ import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import chalk_logo from '../assets/chalk_logo.png'
 import styles from '../styles/NavBar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <Navbar className={styles.NavBar} expand="md" fixed='top'>
             <Container>
+                <NavLink to="/">
                 <Navbar.Brand>
                     <img src={chalk_logo} alt='logo' height='80' />
-                </Navbar.Brand>
+                </Navbar.Brand></NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
-                        <Nav.Link>
+                        <NavLink exact className={styles.Navlink} activeClassName={styles.Active} to="/">
                             <i className='fas fa-home'></i> Home
-                        </Nav.Link>
-                        <Nav.Link>
+                        </NavLink>
+                        <NavLink className={styles.Navlink} activeClassName={styles.Active} to="/signin">
                             <i className='fas fa-sign-in-alt'></i> Sign In
-                        </Nav.Link>
-                        <Nav.Link>
+                        </NavLink>
+                        <NavLink className={styles.Navlink} activeClassName={styles.Active} to="/signup">
                             <i className='fas fa-user-plus'></i> Sign Up
-                        </Nav.Link>
+                        </NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
