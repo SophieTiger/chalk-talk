59751839750c0c097656f770302a02067fc845ef
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-11-19 10:14:41 +0000

    Refactor usercontext code with hooks into CurrentUserContext.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { CurrentUserprovider } from './contexts/CurrentUserContext';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <CurrentUserprovider>
+        <App />
+      </CurrentUserprovider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
