7c8e6e506a5189adb9a93b5ba1d656f74f6f5d05
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-11-19 10:49:44 +0000

    Add request and response axios interceptors

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://chalk-talk-api-7f804e82f4b9.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
