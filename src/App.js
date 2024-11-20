8a7aa38b0c1fb6eb9954da5d0b2ef445e455c7f7
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-11-20 14:45:54 +0000

    Add functionality to display the list of posts on the homepage and added filters

--- src/App.js
+++ src/App.js
@@ -7,15 +7,37 @@ import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
 import PostCreateForm from './pages/posts/PostCreateForm';
 import PostPage from './pages/PostPage';
+import PostsPage from './pages/posts/PostsPage';
+import { useCurrentUser } from './contexts/CurrentUserContext';
 
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
+
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <PostsPage message="No results found. Please try another search keyword." />
+            )}
+          />
+          <Route
+            exact
+            path="/friends"
+            render={() => (
+              <PostsPage
+                message="No results found. Please try another search keyword or follow a new friend."
+                filter={`owner__followed__owner__profile=${profile_id}&`}
+              />
+            )}
+          />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
