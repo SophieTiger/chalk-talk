1d97135686c1b0adab9aa19a1f59f600ae7fea35
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-11-22 15:34:28 +0000

    Add functionality to follow profiles and update the count accordingly

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -1,9 +1,10 @@
 import { createContext, useContext, useState, useEffect } from "react";
 import { axiosReq, axiosRes } from '../api/axiosDefaults';
 import { useCurrentUser } from "./CurrentUserContext";
+import { followHelper } from "../utils/utils";
 
-export const ProfileDataContext = createContext();
-export const SetProfileDataContext = createContext();
+const ProfileDataContext = createContext();
+const SetProfileDataContext = createContext();
 
 export const useProfileData = () => useContext(ProfileDataContext);
 export const useSetProfileData = () => useContext(SetProfileDataContext);
@@ -18,37 +19,52 @@ export const ProfileDataProvider = ({ children }) => {
 
     const handleFollow = async (clickedProfile) => {
         try {
-            const { data } = await axiosRes.post("/followers/", {
-                followed: clickedProfile.id,
-            });
+          const { data } = await axiosRes.post("/followers/", {
+            followed: clickedProfile.id,
+          });
+    
+          setProfileData((prevState) => ({
+            ...prevState,
+            pageProfile: {
+              results: prevState.pageProfile.results.map((profile) =>
+                followHelper(profile, clickedProfile, data.id)
+              ),
+            },
+            popularProfiles: {
+              ...prevState.popularProfiles,
+              results: prevState.popularProfiles.results.map((profile) =>
+                followHelper(profile, clickedProfile, data.id)
+              ),
+            },
+          }));
         } catch (err) {
-            console.log(err);
+          console.log(err);
         }
-
-        useEffect(() => {
-            const handleMount = async () => {
-                try {
-                    const { data } = await axiosReq.get(
-                        "/profiles/?ordering=-followers_count"
-                    );
-                    setProfileData((prevState) => ({
-                        ...prevState,
-                        popularProfiles: data,
-                    }));
-                } catch (err) {
-                    console.log(err);
-                }
-            };
-
-            handleMount();
-        }, [currentUser]);
-
-        return (
-            <ProfileDataContext.Provider value={profileData}>
-                <SetProfileDataContext.Provider value={{setProfileData, handleFollow}}>
-                    {children}
-                </SetProfileDataContext.Provider>
-            </ProfileDataContext.Provider>
-        );
+      };
+    
+      useEffect(() => {
+        const handleMount = async () => {
+          try {
+            const { data } = await axiosReq.get(
+              "/profiles/?ordering=-followers_count"
+            );
+            setProfileData((prevState) => ({
+              ...prevState,
+              popularProfiles: data,
+            }));
+          } catch (err) {
+            console.log(err);
+          }
+        };
+    
+        handleMount();
+      }, [currentUser]);
+    
+      return (
+        <ProfileDataContext.Provider value={profileData}>
+          <SetProfileDataContext.Provider value={{ setProfileData, handleFollow }}>
+            {children}
+          </SetProfileDataContext.Provider>
+        </ProfileDataContext.Provider>
+      );
     };
-}
